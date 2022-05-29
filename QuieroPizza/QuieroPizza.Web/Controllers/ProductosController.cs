587ad27e9dfca47e8f341da8cb610adaef90eb1a
DELL <DELL@DESKTOP-R80K40I>
2022-05-29 16:45:19 -0600

    Agregamos  una lista de productos

---
+++ QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuieroPizza.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto1 = new ProductoModel();
+            producto1.Id = 1;
+            producto1.Descripcion = "Pizza 6 Quesos";
+          
+
+            var producto2 = new ProductoModel();
+            producto2.Id = 2;
+            producto2.Descripcion = "Pizza Estaciones";
+         
+
+            var producto3 = new ProductoModel();
+            producto3.Id = 3;
+            producto3.Descripcion = "Pizza Jamon y Queso";
+
+            var ListaProductos = new List<ProductoModel>();
+            ListaProductos.Add(producto1);
+            ListaProductos.Add(producto2);
+            ListaProductos.Add(producto3);
+            return View(ListaProductos);
+        }
+    }
+}
